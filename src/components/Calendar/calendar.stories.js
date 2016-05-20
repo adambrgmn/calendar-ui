fd605dd829dc0eccb62dcd77b1c42eaf50e273a9
Adam Bergman <adam@fransvilhelm.com>
2016-05-20 12:46:17 +0200

    Updated stories

--- src/components/Calendar/calendar.stories.js
+++ src/components/Calendar/calendar.stories.js
@@ -5,8 +5,10 @@ import moment from 'moment';
 moment.locale('sv');
 
 import Calendar from './index.js';
-import Month from './Month';
 import Header from './Header';
+import Month from './Month';
+import Week from './Week';
+import Day from './Day';
 
 storiesOf('Calendar', module)
   .add('Normal', () => <Calendar />)
@@ -18,8 +20,12 @@ storiesOf('Calendar', module)
   .add('Return custom', () => <Calendar returnAs="DD MMMM YYYY" handleChange={action()} />)
   .add('ReturnAs undefined', () => <Calendar handleChange={action()} />);
 
+storiesOf('Calendar.Header', module)
+  .add('Normal', () => <Header moment={moment()} />)
+  .add('Witch click handler', () => <Header moment={moment()} handleClick={action()} />);
+
 storiesOf('Calendar.Month', module)
-  .add('This month', () => <Month moment={moment()} />)
+  .add('Normal', () => <Month moment={moment()} />)
   .add('Previous month', () => <Month moment={moment().add(-1, 'month')} />)
   .add('Next month', () => <Month moment={moment().add(1, 'month')} />)
   .add('With range', () => (
@@ -35,6 +41,64 @@ storiesOf('Calendar.Month', module)
     <Month moment={moment()} handleChange={action()} />
   ));
 
-storiesOf('Calendar.Header', module)
-  .add('Normal', () => <Header moment={moment()} />)
-  .add('Witch click handler', () => <Header moment={moment()} handleClick={action()} />);
+const days = [
+  moment(),
+  moment().add(1, 'd'),
+  moment().add(2, 'd'),
+  moment().add(3, 'd'),
+  moment().add(4, 'd'),
+  moment().add(5, 'd'),
+  moment().add(6, 'd'),
+];
+
+storiesOf('Calendar.Week', module)
+  .add('Normal', () => <Week monthInView={moment()} days={days} />)
+  .add('With range', () => (
+    <Week monthInView={moment()} days={days} range={[moment().add(1, 'd'), moment().add(3, 'd')]} />
+  ));
+
+const ulStyle = {
+  display: 'flex',
+  justifyContent: 'flex-start',
+  width: '100%',
+  margin: 0,
+  padding: 0,
+  listStyle: 'none',
+};
+
+storiesOf('Calendar.Day', module)
+  .addDecorator((story) => (
+    <ul style={ulStyle}>
+      {story()}
+    </ul>
+  ))
+  .add('Normal', () => <Day monthInView={moment()} day={moment().add(1, 'd')} />)
+  .add('Today', () => <Day monthInView={moment()} day={moment()} />)
+  .add('In range', () => (
+    <Day
+      monthInView={moment()}
+      day={moment()}
+      range={[moment().add(-1, 'd'), moment().add(1, 'd')]}
+    />
+  ))
+  .add('Start of range', () => (
+    <Day
+      monthInView={moment()}
+      day={moment()}
+      range={[moment(), moment().add(1, 'd')]}
+    />
+  ))
+  .add('End of range', () => (
+    <Day
+      monthInView={moment()}
+      day={moment()}
+      range={[moment().subtract(1, 'd'), moment()]}
+    />
+  ))
+  .add('Only in range', () => (
+    <Day
+      monthInView={moment()}
+      day={moment()}
+      range={[moment(), moment()]}
+    />
+  ));
